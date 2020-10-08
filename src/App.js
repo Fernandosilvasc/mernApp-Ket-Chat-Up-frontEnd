bff333f3f89b7f3f1787da8a65d1fbceb0fc1fd8
Fernando Corrêa da Silva <fernando_silvasc@hotmail.com>
2020-10-08 09:32:04 -0700

    created template files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Axios from "axios";
+
+import Header from "./components/layout/Header";
+import Home from "./components/pages/Home";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import UserContext from "./context/UserContext";
+
+import "./style.css";
 
 function App() {
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
+
+  useEffect(() => {
+    const checkLogdedIn = async () => {
+      let token = localStorage.getItem("auth-token");
+      if (token === null) {
+        localStorage.setItem("auth-token", "");
+        token = "";
+      }
+
+      const tokenRes = await Axios.post(
+        "http://localhost:8000/users/tokenIsValid",
+        null,
+        {
+          headers: { "x-auth-token": token },
+        }
+      );
+
+      if (tokenRes.data) {
+
+        const userRes = await Axios.get(
+          "http://localhost:8000/users/", {
+          headers: { "x-auth-token": token },
+        });
+
+        setUserData({
+          token: token,
+          user: userRes.data,
+        });
+      }
+    };
+    checkLogdedIn();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home}></Route>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/register' component={Register}></Route>
+        </Switch>
+      </UserContext.Provider>
+    </BrowserRouter>
   );
 }
 
