daf2073ab5dbcc89c454bc5bafccf770ad0ea58e
Fernando Corrêa da Silva <fernando_silvasc@hotmail.com>
2020-10-09 11:36:57 -0700

    fixed some issues from mergin

--- src/App.js
+++ src/App.js
@@ -1,79 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Axios from "axios";
 
 import Header from "./components/layout/Header";
-import Nav from './components/layout/Nav';
+import Nav from "./components/layout/Nav";
 import Home from "./components/pages/Home";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import UserContext from "./context/UserContext";
-import Dashboard from "./components/pages/dashboard/Dashboard"
+import Dashboard from "./components/pages/dashboard/Dashboard";
 
-import TEST from "./components/layout/test"
+import TEST from "./components/layout/test";
 
-
-import './style.css';
+import "./style.css";
 
 function App() {
-    const [userData, setUserData] = useState({
-        token: undefined,
-        user: undefined
-    });
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
 
-    useEffect(() => {
-        const checkLogdedIn = async() => {
-            let token = localStorage.getItem('auth-token');
-            if (token === null) {
-                localStorage.setItem('auth-token', '');
-                token = '';
-            }
+  useEffect(() => {
+    const checkLogdedIn = async () => {
+      let token = localStorage.getItem("auth-token");
+      if (token === null) {
+        localStorage.setItem("auth-token", "");
+        token = "";
+      }
 
-            const tokenRes = await Axios.post('http://localhost:8000/users/tokenIsValid', null, {
-                headers: { 'x-auth-token': token }
-            });
-
-            if (tokenRes.data) {
-                const userRes = await Axios.get('http://localhost:8000/users/', {
-                    headers: { 'x-auth-token': token }
-                });
-
-                setUserData({
-                    token: token,
-                    user: userRes.data
-                });
-            }
-        };
-        checkLogdedIn();
-    }, []);
-
-    return ( <
-        BrowserRouter >
-        <
-        UserContext.Provider value = {
-            { userData, setUserData } } >
-        <
-        Header / >
-        <
-        Nav / >
-        <
-        Switch >
-        <
-        Route exact path = "/"
-        component = { Home }
-        /> <
-        Route path = "/login"
-        component = { Login }
-        /> <
-        Route path = "/register"
-        component = { Register }
-        /> <
-        Home / >
-        <
-        /Switch> <
-        /UserContext.Provider> <
-        /BrowserRouter>
-    );
+      const tokenRes = await Axios.post(
+        "http://localhost:8000/users/tokenIsValid",
+        null,
+        {
+          headers: { "x-auth-token": token },
+        }
+      );
 
       if (tokenRes.data) {
         const userRes = await Axios.get("http://localhost:8000/users/", {
@@ -93,18 +54,16 @@ function App() {
     <BrowserRouter>
       <UserContext.Provider value={{ userData, setUserData }}>
         <Header />
-        <TEST></TEST>
+        <Nav />
         <Switch>
-          <Route exact path='/' component={Home}></Route>
-          <Route path='/login' component={Login}></Route>
-          <Route path='/register' component={Register}></Route>
-          <Route path='/dashboard' component={Dashboard}></Route>
+          <Route exact path='/' component={Home} />
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
           <Home />
         </Switch>
       </UserContext.Provider>
     </BrowserRouter>
   );
-
 }
 
 export default App;
--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -39,56 +39,7 @@ export default function Register() {
         }
     };
 
-  return (
-    <>
-    <div className="login-container">
 
-    <div className="login">
-      <div className="splash">
-<img src={splash} alt=""/>
-      </div>
-      <div className="avatar">
-        <img src={avatar} alt=""/>
-      </div>
-      
-      {error && (
-        <ErrorNotice message={error} clearError={() => setError(undefined)} />
-      )}
-      <form className="form" onSubmit={submit}>
-  
-        <input
-          id="login-email"
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="email"
-        />
-    
-              <label htmlFor="login-email">Forgot e-mail?</label>
-        <br/>
-      
-      
-        <input
-          id="login-password"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="password"
-        />
-   
-        <label htmlFor="login-password">Forgot password?</label>
-        <br/>
-        <br/>
-        <input type="submit" className="login-btn" value="Log In" />
-        <div className="join">
-        <a href="Register.jsx">
-        <h1>NOT A MEMBER YET? JOIN TODAY!</h1>
-        </a>
-      </div>
-      </form>
-    
-    
-      <div className="splash2">
-        <img src={splash2} alt=""/>
-      </div>
 
     return (
         <>
