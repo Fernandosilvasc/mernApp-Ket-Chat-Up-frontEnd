c803388f8b293b79cbf1819313bc41ebb22a07ff
Fernando Corrêa da Silva <fernando_silvasc@hotmail.com>
2020-10-12 20:35:00 -0700

    fixed props in Join.jsx

--- src/components/testsocket/Join.jsx
+++ src/components/testsocket/Join.jsx
@@ -1,20 +1,35 @@
-import React, { useState, useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
 import UserContext from "../../context/UserContext";
 
 const Join = () => {
     // const [user, setUser] = useState('')
     const { userData } = useContext(UserContext);
-    const [username, setUsername] = useState("")
-    const [room, setRoom] = useState('');
+    const [username, setUsername] = useState("");
+    const [room, setRoom] = useState("");
+
+    useEffect(() => {
+        if (userData.user) {
+            setUsername(userData.user.userName);
+        }
+    }, [userData]);
 
     return (
         <div>
             JOIN TEST
-            <input placeholder="Room" type="text" onChange={(event) => setRoom(event.target.value)} />
-            <Link onClick={event => (!room) ? event.preventDefault() : null} to={`/chat?room=${room}&username=${username}`}>
-                <button type="submit">Join</button>
-            </Link>
+            <input
+                placeholder="Room"
+                type="text"
+                onChange={(event) => setRoom(event.target.value)}
+            />
+            {username && (
+                <Link
+                    onClick={(event) => (!room ? event.preventDefault() : null)}
+                    to={`/chat?room=${room}&username=${username}`}
+                >
+                    <button type="submit">Join</button>
+                </Link>
+            )}
         </div>
     );
 };
